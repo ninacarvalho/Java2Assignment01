82190ba180c880fc9b7edaa27ea974b310e2b646
Marina <marinacarvalho@protonmail.com>
2024-01-28 16:54:28 -0500

    isolating side input on a method

--- src/task1/TestTriangle.java
+++ src/task1/TestTriangle.java
@@ -12,10 +12,10 @@ Date:<submission date>
 
 package task1;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class TestTriangle {
-
 	public static void main(String[] args) {
 
 		// call the method to create a triangle		
@@ -24,6 +24,7 @@ public class TestTriangle {
 	}
 	
 	// method to create the triangle
+
 	public static Triangle createTriangle() {
 		
 		Scanner kbd = new Scanner(System.in);
@@ -35,37 +36,14 @@ public class TestTriangle {
 		// color, and a Boolean value to indicate whether the triangle
 		// is filled.
 		System.out.println("Enter three sides of the triangle: ");
-		
-		do {
-			System.out.print("Side 1: ");
-			while(!kbd.hasNextDouble()) {
-				System.out.println("Sides have to be a positive number");
-				kbd.next();
-			}
-			side1 = kbd.nextDouble();
-		}while(side1 <= 0);
-		
-		do {
-			System.out.print("Side 2: ");
-			while(!kbd.hasNextDouble()) {
-				System.out.println("Sides have to be a positive number");
-				kbd.next();
-			}
-			side2 = kbd.nextDouble();
-		}while(side2 <= 0);
-		
-		do {
-			System.out.print("Side 3: ");
-			while(!kbd.hasNextDouble()) {
-				System.out.println("Sides have to be a positive number");
-				kbd.next();
-			}
-			side3 = kbd.nextDouble();
-		}while(side3 <= 0);
-				
+
+		side1 = defineSide("Side 1: ", kbd);
+		side2 = defineSide("Side 2: ", kbd);
+		side3 = defineSide("Side 3: ", kbd);
+
 		System.out.print("Enter filled color: ");
 		String color = kbd.next();
-		
+
 		do {
 			System.out.print("Is the triangle filled? Enter \"true\" for yes, \"false\" for no: ");
 			while(!kbd.hasNextBoolean()) {
@@ -74,18 +52,38 @@ public class TestTriangle {
 			}
 			filled = kbd.nextBoolean();
 			go = false;
-		}while(go);
-				
+		} while(go);
+
 		// create a Triangle object with the given input
 		Triangle triangle = new Triangle(side1, side2, side3);
 		triangle.setColor(color);
 		triangle.setFilled(filled);
-		
+
 		kbd.close();
-		
+
 		return triangle;
 	}
-	
+
+	private static double defineSide(String message, Scanner scan) {
+		boolean sideIsValid = false;
+		double side = -1;
+
+		while(!sideIsValid) {
+			System.out.println(message);
+			String input = scan.nextLine();
+			try {
+				side = Double.parseDouble(input);
+				if (side <= 0) {
+					System.err.printf("Side must be greater than 0.\n");
+				} else {
+					sideIsValid = true;
+				}
+			} catch (Exception e) {
+				System.err.println("Invalid input. Please type in a positive number");
+			}
+		}
+		return side;
+	}
 }
 
 
