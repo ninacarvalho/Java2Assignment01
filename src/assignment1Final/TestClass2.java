e5a26e313e5cef68de0a218c6f46eff6a3574fe8
Desmond <142507179+DESMONDSYU@users.noreply.github.com>
2024-01-30 16:26:42 -0500

    Update TestClass2.java

--- src/assignment1Final/TestClass2.java
+++ src/assignment1Final/TestClass2.java
@@ -1,12 +1,7 @@
 package assignment1Final;
 
 public class TestClass2 {
-//	public static void main(String[] args) throws TriangleException {
-//        Triangle2 validTriangle = new Triangle2(2,3,4);
-//        System.out.println(validTriangle.toString());
-//        Triangle2 invalidTriangle = new Triangle2(1,44,1);
-//  }	
-	
+
 	public static void main(String[] args) {		
 
 		try {
